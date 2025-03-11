2b9e4e70f7b467dd39b9f99ee0379101237b483f
hgs3 <82091+hgs3@users.noreply.github.com>
2025-03-10 20:13:49 -0500

    First commit of Confetti website.

---
+++ assets/confetti.js
@@ -0,0 +1,2 @@
+!function(){"use strict";const e=document.getElementById("menu-toggle"),t=document.getElementById("menu-overlay");e.addEventListener("change",(e=>{e.target.checked?t.classList.add("menu-overlay"):t.classList.remove("menu-overlay")})),t.addEventListener("click",(()=>{e.checked=!1,t.classList.remove("menu-overlay")}))}();
+//# sourceMappingURL=confetti.js.map
